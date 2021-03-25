fc71cdc5053bdc8c9333f9300ea45d6bfe3cd4dd
Javi <javierplazasisques@gmail.com>
2021-03-25 11:35:44 +0100

    Añadida función delay

--- Coche_Fantastico/main.asm
+++ Coche_Fantastico/main.asm
@@ -1,3 +1,7 @@
+/**************************************************************************
+	Autor: Javier Plaza Sisqués
+***************************************************************************/
+
 
 SER R16											;Inicializamos R16 con todo unos
 OUT DDRD, R16									;Inicializamos puerto D como salida
@@ -6,23 +10,7 @@ LDI R16, 1										;Guardamos el valor 1 en R16
 
 izquierda: 
 	
-; Assembly code auto-generated
-; by utility from Bret Mulvey
-; Delay 1 600 000 cycles
-; 100ms at 16 MHz
-
-    ldi  r20, 9
-    ldi  r21, 30
-    ldi  r22, 229
-L1: dec  r22
-    brne L1
-    dec  r21
-    brne L1
-    dec  r20
-    brne L1
-    nop
-
-
+	CALL delay_100								;Llamamos a la funcion delay
 
 	OUT PORTD, R16								;Sacamos R16 por los pines D
 	LSL R16										;Multiplicamos por 2 el numero binario para cambiar el LED
@@ -32,28 +20,41 @@ L1: dec  r22
 	
 derecha:
 
-		
-; Assembly code auto-generated
-; by utility from Bret Mulvey
-; Delay 1 600 000 cycles
-; 100ms at 16 MHz
+	CALL delay_100								;Llamamos a la funcion delay
 
-    ldi  r20, 9
-    ldi  r21, 30
-    ldi  r22, 229
-L2: dec  r22
-    brne L2
-    dec  r21
-    brne L2
-    dec  r20
-    brne L2
-    nop
-
-
-	
 	OUT PORTD, R16								;Sacamos R16 por los pines D
 	LSR R16										;Dividimos entre 2 el numero binario para cambiar el LED
 	CPI R16, 1									;Comparamos R16 con 1
 	BREQ izquierda								;Si lo anterior es igual accedemos a la etiqueta izquierda
 	RJMP derecha								;Saltamos a la etiqueta derecha
 
+
+/**************************************************************************
+
+	Fin del programa principal
+
+***************************************************************************/
+
+/**************************************************************************
+
+	Funcion Delay 100ms
+
+***************************************************************************/
+delay_100:
+	; Assembly code auto-generated
+	; by utility from Bret Mulvey
+	; Delay 1 600 000 cycles
+	; 100ms at 16 MHz
+
+		ldi  r20, 9
+		ldi  r21, 30
+		ldi  r22, 229
+	L1: dec  r22
+		brne L1
+		dec  r21
+		brne L1
+		dec  r20
+		brne L1
+		nop
+
+	RET											;RET es el equivalente al return, antes deberemos de haber guardado el PC (Program Counter)		
